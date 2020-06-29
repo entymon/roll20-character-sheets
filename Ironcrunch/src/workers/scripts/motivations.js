70e72ed0c582236254cf29a7c511087eea2c38a9
LeeRhodes <leerhodes@protonmail.com>
2020-06-29 18:18:02 +0100

    updated motivations

--- Ironcrunch/src/workers/scripts/motivations.js
+++ Ironcrunch/src/workers/scripts/motivations.js
@@ -1,6 +1,18 @@
+function cleanInput (value) {
+  return value.toLowerCase().replace(/\s+/g, '_')
+}
+
 on('change:repeating_motivations:motivation_select', function(values) {
+  const motivationOld = cleanInput(values.previousValue)
+  const motivationNew = cleanInput(values.newValue)
   setAttrs({
-      ['repeating_motivations_motivation_' + values.previousValue]: 'off',
-      ['repeating_motivations_motivation_' + values.newValue]: 'on'
+    ['repeating_motivations_motivation_' + motivationOld]: 'off',
+    ['repeating_motivations_motivation_' + motivationNew]: 'on'
+  });
+});
+
+on('change:repeating_motivations:details_button', function(values) {
+  setAttrs({
+    repeating_motivations_details: values.newValue
   });
 });
